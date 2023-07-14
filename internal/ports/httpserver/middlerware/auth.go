ffe892bdaadbe44c1a2ce024a078ac4a3bd3aa95
Vaayne <liu.vaayne@gmail.com>
2023-07-14 23:16:00 +0800

    fix auth by apikey

--- internal/ports/httpserver/middlerware/auth.go
+++ internal/ports/httpserver/middlerware/auth.go
@@ -1,7 +1,7 @@
 package middlerware
 
 import (
-	"net/http"
+	"strings"
 
 	"openai-dashboard/internal/pkg/logger"
 
@@ -10,29 +10,27 @@ import (
 	"github.com/pocketbase/pocketbase/daos"
 )
 
-const (
-	ApiKeyHeader         string = "X-API-KEY"
-	ApiKeyCollectionName string = "api_keys"
-)
+const ApiKeyCollectionName = "api_keys"
 
-// AuthByApiKeyMiddleware
+// AuthByApiKeyMiddleware is a middleware to auth user by api key
 func AuthByApiKeyMiddleware(dao *daos.Dao) echo.MiddlewareFunc {
 	return func(next echo.HandlerFunc) echo.HandlerFunc {
 		return func(c echo.Context) error {
-			auth := c.Request().Header.Get(ApiKeyHeader)
-			if auth != "" {
-				record, err := dao.FindFirstRecordByData(ApiKeyCollectionName, "key", auth)
-				if err != nil {
-					logger.SugaredLogger.Infow("error get user by api key", "err", err, "key", auth)
-					return apis.NewUnauthorizedError("invalid api key", nil)
+			val := c.Get(apis.ContextAuthRecordKey)
+			if val == nil {
+				auth := c.Request().Header.Get("Authorization")
+				if auth != "" {
+					auths := strings.Split(auth, " ")
+					if len(auths) == 2 && auths[0] == "Bearer" {
+						apiKey := auths[1]
+						authRecord, err := dao.FindFirstRecordByData(ApiKeyCollectionName, "key", apiKey)
+						if err != nil {
+							logger.SugaredLogger.Infow("error get user by api key", "err", err, "key", auth)
+							return apis.NewUnauthorizedError("invalid api key", nil)
+						}
+						c.Set(apis.ContextAuthRecordKey, authRecord)
+					}
 				}
-				userId := record.GetString("user_id")
-				authRecord, err := dao.FindRecordById("users", userId)
-				if err != nil {
-					logger.SugaredLogger.Errorw("error get user by id", "err", err, "user_id", userId)
-					return apis.NewApiError(http.StatusInternalServerError, "api key auth error", nil)
-				}
-				c.Set(apis.ContextAuthRecordKey, authRecord)
 			}
 			return next(c)
 		}
