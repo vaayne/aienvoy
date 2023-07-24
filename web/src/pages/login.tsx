d7b6070b789b090cf9a3fdea27dcb34ae8bf7037
Vaayne <liu.vaayne@gmail.com>
2023-07-24 10:31:57 +0800

    use react file base route and auto formatter

---
+++ web/src/pages/login.tsx
@@ -0,0 +1,11 @@
+export default function Login() {
+  return (
+    <>
+      <div className="container mx-auto p-10">
+        <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
+          <h1 className="text-6xl font-bold">Login Page</h1>
+        </main>
+      </div>
+    </>
+  );
+}
--- web/vite.config.ts
+++ web/vite.config.ts
@@ -4,6 +4,11 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+	server: {
+		host: "0.0.0.0",
+		port: 3000,
+		strictPort: true,
+	},
 	plugins: [react()],
 	resolve: {
 		alias: {
