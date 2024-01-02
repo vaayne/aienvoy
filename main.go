4e179215a024f3bf6257105b57fe9b1d043e6980
Vaayne <liu.vaayne@gmail.com>
2024-01-02 18:23:47 +0800

    refactor main method

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"embed"
 	"log/slog"
+	"os/exec"
+	"runtime"
 
 	"github.com/Vaayne/aienvoy/internal/core/midjourney"
 	"github.com/Vaayne/aienvoy/internal/core/readease"
@@ -22,40 +24,16 @@ import (
 //go:embed all:web
 var staticFiles embed.FS
 
-func registerRoutes(app *pocketbase.PocketBase) {
+// RegisterRoutes registers the HTTP routes for the application.
+func RegisterRoutes(app *pocketbase.PocketBase) {
 	app.OnBeforeServe().Add(func(e *core.ServeEvent) error {
 		httpserver.RegisterRoutes(e.Router, app, staticFiles)
 		return nil
 	})
 }
 
-// func openBrowser(url string) error {
-// 	var cmd string
-// 	var args []string
-
-// 	switch runtime.GOOS {
-// 	case "windows":
-// 		cmd = "cmd"
-// 		args = []string{"/c", "start", url}
-// 	case "darwin":
-// 		cmd = "open"
-// 		args = []string{url}
-// 	default: // "linux", "freebsd", "openbsd", "netbsd"
-// 		cmd = "xdg-open"
-// 		args = []string{url}
-// 	}
-
-// 	return exec.Command(cmd, args...).Start()
-// }
-
-func main() {
-	app := pocketbase.New()
-	// migrate DB
-	migratecmd.MustRegister(app, app.RootCmd, &migratecmd.Options{
-		Automigrate: false,
-	})
-
-	// scheduled jobs
+// SetScheduledJobs sets up the scheduled jobs for the application.
+func SetScheduledJobs(app *pocketbase.PocketBase) {
 	app.OnBeforeServe().Add(func(e *core.ServeEvent) error {
 		scheduler := cron.New()
 		// hourly readease job
@@ -78,27 +56,66 @@ func main() {
 		scheduler.Start()
 		return nil
 	})
-	// register routes
-	registerRoutes(app)
+}
 
-	// after bootstrap start other service
-	app.OnAfterBootstrap().Add(func(e *core.BootstrapEvent) error {
-		// start telegram bot
-		go tgbot.Serve(app)
-		// start midjourney bot
-		go func() {
-			if config.GetConfig().MidJourney.DiscordBotToken != "" {
-				m := midjourney.New(app.Dao())
-				m.Client.Serve()
-			}
-		}()
+// StartTelegramBot starts the Telegram bot for the application.
+func StartTelegramBot(app *pocketbase.PocketBase) {
+	app.OnBeforeServe().Add(func(e *core.ServeEvent) error {
+		if config.GetConfig().Telegram.Token != "" {
+			go tgbot.Serve(app)
+		}
 		return nil
 	})
-	// go func() {
-	// 	if err := openBrowser(config.GetConfig().Service.URL); err != nil {
-	// 		slog.Error("Open browser error", "err", err)
-	// 	}
-	// }()
+}
+
+// StartMidjourneyServer starts the Midjourney server for the application.
+func StartMidjourneyServer(app *pocketbase.PocketBase) {
+	app.OnBeforeServe().Add(func(e *core.ServeEvent) error {
+		if config.GetConfig().MidJourney.DiscordBotToken != "" {
+			m := midjourney.New(app.Dao())
+			m.Client.Serve()
+		}
+		return nil
+	})
+}
+
+// OpenBrowser opens the default web browser with the specified URL.
+func OpenBrowser(url string) {
+	var cmd string
+	var args []string
+
+	switch runtime.GOOS {
+	case "windows":
+		cmd = "cmd"
+		args = []string{"/c", "start", url}
+	case "darwin":
+		cmd = "open"
+		args = []string{url}
+	default: // "linux", "freebsd", "openbsd", "netbsd"
+		cmd = "xdg-open"
+		args = []string{url}
+	}
+
+	if err := exec.Command(cmd, args...).Start(); err != nil {
+		slog.Error("Open browser error", "err", err)
+	}
+}
+
+func main() {
+	app := pocketbase.New()
+	// migrate DB
+	migratecmd.MustRegister(app, app.RootCmd, &migratecmd.Options{
+		Automigrate: false,
+	})
+
+	// before serve hooks
+	RegisterRoutes(app)
+	StartTelegramBot(app)
+	StartMidjourneyServer(app)
+	// SetScheduledJobs(app)
+	// OpenBrowser(config.GetConfig().Service.URL)
+
+	// start app
 	if err := app.Start(); err != nil {
 		slog.Error("failed to start app", "err", err)
 	}
